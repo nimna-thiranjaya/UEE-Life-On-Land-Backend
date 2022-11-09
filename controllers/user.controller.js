12425f448dfbce009298719a021b1142d9332337
Nimna Thiranjaya <nimnathiranjya523@gmail.com>
2022-11-09 09:57:45 +0530

    ADD : (Admin, User Profile mgt)

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user.model");
 
@@ -23,11 +24,11 @@ const UserRegister = async (req, res) => {
       const result = await User.create(newUser);
 
       if (result) {
-        res
+        return res
           .status(201)
           .send({ status: true, message: "User Created Successfully" });
       } else {
-        res
+        return res
           .status(400)
           .send({ status: false, message: "User Creation Failed" });
       }
@@ -37,8 +38,171 @@ const UserRegister = async (req, res) => {
         .send({ status: false, message: "User already exists" });
     }
   } catch (error) {
-    res.status(400).send({ status: false, message: error.message });
+    return res.status(400).send({ status: false, message: error.message });
   }
 };
 
-module.exports = { UserRegister };
+//Upload profile image
+const UploadProfileImage = async (req, res) => {
+  try {
+    const { profileImage } = req.body;
+
+    const user = req.logedUser;
+
+    const result = await User.findByIdAndUpdate(user._id, {
+      profileImage: profileImage,
+    });
+
+    if (result) {
+      return res
+        .status(201)
+        .send({ status: true, message: "Profile Image Uploaded" });
+    } else {
+      return res
+        .status(400)
+        .send({ status: false, message: "Profile Image Upload Failed" });
+    }
+  } catch (e) {
+    return res.status(500).send({ status: false, message: e.message });
+  }
+};
+
+//User login
+const UserLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const isEmailCheck = await User.findOne({ email: email });
+
+    if (isEmailCheck) {
+      const isPasswordCheck = await bcrypt.compare(
+        password,
+        isEmailCheck.password
+      );
+
+      if (isPasswordCheck) {
+        const token = jwt.sign(
+          { _id: isEmailCheck._id },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: "2d",
+          }
+        );
+
+        const tokenarraysize = isEmailCheck.tokens.length;
+
+        if (tokenarraysize >= 5) {
+          const temp = [];
+          await User.findByIdAndUpdate(isEmailCheck._id, { tokens: temp });
+        }
+
+        const tokens = {
+          token: token,
+        };
+
+        await User.findByIdAndUpdate(isEmailCheck._id, {
+          $push: { tokens: tokens },
+        });
+
+        return res.status(200).send({
+          status: true,
+          message: "Login Successfull",
+          token: token,
+          role: isEmailCheck.role,
+        });
+      } else {
+        return res
+          .status(400)
+          .send({ status: false, message: "Invalid Password" });
+      }
+    } else {
+      return res
+        .status(400)
+        .send({ status: false, message: "Account not found" });
+    }
+  } catch (e) {
+    return res.status(500).send({ status: false, message: e.message });
+  }
+};
+
+//User Profile
+const UserProfile = async (req, res) => {
+  try {
+    const LogedUser = req.logedUser;
+    if (LogedUser) {
+      return res.status(200).send({ status: true, user: LogedUser });
+    } else {
+      return res.status(400).send({ status: false, message: "User not found" });
+    }
+  } catch (e) {
+    return res.status(500).send({ status: false, message: e.message });
+  }
+};
+
+//User profile update
+const UserProfileUpdate = async (req, res) => {
+  try {
+    const { fullName, description, phoneNumber, Occupation } = req.body;
+
+    const LogedUser = req.logedUser;
+
+    if (LogedUser) {
+      const newData = {
+        fullName,
+        description,
+        phoneNumber,
+        Occupation,
+      };
+      const result = await User.findByIdAndUpdate(LogedUser._id, newData);
+
+      if (result) {
+        return res
+          .status(201)
+          .send({ status: true, message: "Profile Updated" });
+      } else {
+        return res
+          .status(400)
+          .send({ status: false, message: "Profile Update Failed" });
+      }
+      s;
+    } else {
+      return res.status(400).send({ status: false, message: "User not found" });
+    }
+  } catch (e) {
+    return res.status(500).send({ status: false, message: e.message });
+  }
+};
+
+//Delete User Profile
+const DeleteUserProfile = async (req, res) => {
+  try {
+    const LogedUser = req.logedUser;
+
+    if (LogedUser) {
+      const result = await User.findByIdAndDelete(LogedUser._id);
+
+      if (result) {
+        return res
+          .status(201)
+          .send({ status: true, message: "Profile Deleted" });
+      } else {
+        return res
+          .status(400)
+          .send({ status: false, message: "Profile Delete Failed" });
+      }
+    } else {
+      return res.status(400).send({ status: false, message: "User not found" });
+    }
+  } catch (e) {
+    return res.status(500).send({ status: false, message: e.message });
+  }
+};
+
+module.exports = {
+  UserRegister,
+  UploadProfileImage,
+  UserLogin,
+  UserProfile,
+  UserProfileUpdate,
+  DeleteUserProfile,
+};
--- models/user.model.js
+++ models/user.model.js
@@ -1,44 +1,57 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      trim: true,
+    },
+    Occupation: {
+      type: String,
+      trim: true,
+    },
+    role: {
+      type: String,
+      default: "User",
+      required: true,
+      trim: true,
+    },
+    profileImage: {
+      type: String,
+      trim: true,
+    },
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  phoneNumber: {
-    type: String,
-    trim: true,
-  },
-  Occupation: {
-    type: String,
-    trim: true,
-  },
-  role: {
-    type: String,
-    default: "User",
-    required: true,
-    trim: true,
-  },
-  profileImage: {
-    type: String,
-    trim: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
